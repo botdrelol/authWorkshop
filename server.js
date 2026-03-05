7bd37463900dd2fc85c18f88f22cac89503450dd
Andre Lee <realandre1234@gmail.com>
2026-03-04 17:12:10 -0800

    Day 2 of Auth Workshop

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require ('dotenv').config()
 
 const express = require ('express')
 const app = express()
 const bcrypt = require ('bcrypt')
 const jwt = require ('jsonwebtoken') //import modules
 const saltRounds = 10
-
+const testHash = "$2b$10$VqqJYKM8WLLFDGKMDrrkzO9aR0GKQ4gadBX3hJYUC1gMET1nK3JN6"
 
 app.use (express.json())
 
@@ -32,12 +33,33 @@ app.post ('/login', (req,res)=>{
     const user = {name : username}
 
     //compare password to the hash from db
-    
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json ({accessToken:accessToken}) //send back token to user for user to use later in endpoints
+    async function comparePassword (user, password){
+        const match = bcrypt.compare(password, testHash)
+        if (match){
+            const accessToken = generateAccessToken(user)
+            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET) //async = wait for part of function to complete
+            return res.json({accessToken: accessToken , refreshToken: refreshToken})
+            }
+        return res.sendStatus(403)
+    }
+    return comparePassword(user,password)
 })
 
+app.post ('/token', (req,res)=>{
+    const refreshToken = req.body.token
+
+    if (refreshToken === null) return res.sendStatus(401)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err,user) => {
+        if (err) return res.status(403).send("Failed to verify")
+        const accessToken = generateAccessToken({name : user.name})
+        res.json({accessToken : accessToken})
+    })
+})
 app.listen(8980, (error) => {
     if (error) return console.log (`Server failed to start ${error}`)
     console.log ("Server is listening")
 })
+
+function generateAccessToken(user){
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'5m'})
+}
